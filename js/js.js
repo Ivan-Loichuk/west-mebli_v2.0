e3e9cac9bd15208dd60ab995c5160588a572c6fa
Ivan Loichuk <ivan.loichuk@pollub.edu.pl>
2017-02-27 15:38:50 +0100

    27.02.2017_(15:38) carousel

--- js/js.js
+++ js/js.js
@@ -1,4 +1,18 @@
+//NAV-MENU
+
 var options ={
-	offset: 0
+	offset: 200
 }
 var header = new Headhesive('.nav-menu', options);
+
+
+// Gallery
+$(document).ready(function(){
+    //FANCYBOX
+    //https://github.com/fancyapps/fancyBox
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+   
